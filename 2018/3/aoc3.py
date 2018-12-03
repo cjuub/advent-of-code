e8bfa2391319b35830fda70bdcdbb279e9c4586a
cjuub <cjuuub@gmail.com>
2018-12-03 18:14:45 +0100

    Add solution for day 3.

---
+++ 2018/3/aoc3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+grid = [[0 for _ in range(1000)] for _ in range(1000)]
+
+cnt = 0
+with open('input.txt') as fp:
+    for line in fp:
+        entry = line.split(' ')
+        x0, y0 = tuple([int(x) for x in entry[2][:-1].split(',')])
+        width, height = tuple([int(y) for y in entry[3].split('x')])
+
+        for x in range(x0, x0 + width):
+            for y in range(y0, y0 + height):
+                if grid[x][y] == 1:
+                    cnt += 1
+
+                grid[x][y] += 1
+
+print(cnt)
+
