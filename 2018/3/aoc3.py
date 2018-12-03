a6c77c1105605499315cb0e459959aae1d10c718
cjuub <cjuuub@gmail.com>
2018-12-03 18:27:35 +0100

    Add gold solution for day 3.

--- 2018/3/aoc3.py
+++ 2018/3/aoc3.py
@@ -6,6 +6,7 @@ cnt = 0
 with open('input.txt') as fp:
     for line in fp:
         entry = line.split(' ')
+        claim_id = entry[0]
         x0, y0 = tuple([int(x) for x in entry[2][:-1].split(',')])
         width, height = tuple([int(y) for y in entry[3].split('x')])
 
@@ -15,6 +16,22 @@ with open('input.txt') as fp:
                     cnt += 1
 
                 grid[x][y] += 1
+
+with open('input.txt') as fp:
+    for line in fp:
+        entry = line.split(' ')
+        claim_id = entry[0]
+        x0, y0 = tuple([int(x) for x in entry[2][:-1].split(',')])
+        width, height = tuple([int(y) for y in entry[3].split('x')])
+
+        overlaps = False
+        for x in range(x0, x0 + width):
+            for y in range(y0, y0 + height):
+                if grid[x][y] > 1:
+                    overlaps = True
+
+        if not overlaps:
+            print('Claim not overlapping: ' + claim_id)
 
 print(cnt)
 
